c4e3e75dba1f0e077150ea82dc50bc8aa973b41d
Tony Mathisen <tony2mat@gmail.com>
2025-10-09 08:09:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      // Map alle "lodash/*.js" til "lodash-es/*.js" (Glide 6.0.1 forventer .js-stier)
+      { find: /^lodash\/(.+)\.js$/, replacement: "lodash-es/$1.js" },
+      { find: "lodash", replacement: "lodash-es" }
+    ]
+  },
+  optimizeDeps: {
+    include: ["lodash-es"]
+  },
   server: { port: 5173 },
   build: {
     target: "es2020",
